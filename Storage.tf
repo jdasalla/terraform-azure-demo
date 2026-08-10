e41f265b8d1b106eb88e3712e9a25fc9797f9d2b
Jason Dasalla <azureuser120396@gmail.com>
2026-08-10 20:08:27 +0800

    adding stoage

--- Storage.tf
+++ Storage.tf
@@ -1,8 +1,8 @@
-#terraform {
- # backend "azurerm" {
+terraform {
+ backend "azurerm" {
     #resource_group_name  = "demo-rg"                # RG kung saan mo nilagay ang storage
     #storage_account_name = "terraformdatabasestorage" # pangalan ng storage account mo
-   # container_name       = "tfstate"                # pangalan ng container
-  #  key                  = "terraform.tfstate"
- # }
-#}
+   container_name       = "tfstate"                # pangalan ng container
+    key                  = "terraform.tfstate"
+ }
+}
