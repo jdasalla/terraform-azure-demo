cbfd86149e723a04fffa7af0aee3be2840bc4955
Jason Dasalla <azureuser120396@gmail.com>
2026-08-10 20:06:21 +0800

    deleting storage

--- Storage.tf
+++ Storage.tf
@@ -1,5 +1,5 @@
 #terraform {
-  backend "azurerm" {
+ # backend "azurerm" {
     #resource_group_name  = "demo-rg"                # RG kung saan mo nilagay ang storage
     #storage_account_name = "terraformdatabasestorage" # pangalan ng storage account mo
    # container_name       = "tfstate"                # pangalan ng container
