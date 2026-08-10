02fdad49e61fcc0310e8117e47eab732693d4bdd
Jason Dasalla <azureuser120396@gmail.com>
2026-08-10 22:31:48 +0800

    adding port 443 to nsg

--- Storage.tf
+++ Storage.tf
@@ -1,8 +1,9 @@
 terraform {
  backend "azurerm" {
-    #resource_group_name  = "demo-rg"                # RG kung saan mo nilagay ang storage
-    #storage_account_name = "terraformdatabasestorage" # pangalan ng storage account mo
+    resource_group_name  = "demo-rg"                # RG kung saan mo nilagay ang storage
+    storage_account_name = "terraformdatabasestorage" # pangalan ng storage account mo
    container_name       = "tfstate"                # pangalan ng container
     key                  = "terraform.tfstate"
  }
 }
+
