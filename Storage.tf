d5d15f14cb0f45ee546fb90b45b94c7910fee958
Jason Dasalla <azureuser120396@gmail.com>
2026-08-11 13:46:09 +0800

    adding dns label to demo-vm1

--- Storage.tf
+++ Storage.tf
@@ -1,9 +1,9 @@
 terraform {
- backend "azurerm" {
-    resource_group_name  = "demo-rg"                # RG kung saan mo nilagay ang storage
+  backend "azurerm" {
+    resource_group_name  = "demo-rg"                  # RG kung saan mo nilagay ang storage
     storage_account_name = "terraformdatabasestorage" # pangalan ng storage account mo
-   container_name       = "tfstate"                # pangalan ng container
+    container_name       = "tfstate"                  # pangalan ng container
     key                  = "terraform.tfstate"
- }
+  }
 }
 
--- VNET.tf
+++ VNET.tf
@@ -33,6 +33,11 @@ resource "azurerm_public_ip" "vm_ip" {
   location            = azurerm_resource_group.RG_Terraform.location
   resource_group_name = azurerm_resource_group.RG_Terraform.name
   allocation_method   = "Static"
+
+  dns_settings {
+    domain_name_label = "demo-vm1"
+  }
+
 }
 
 resource "azurerm_network_interface" "nic" {
